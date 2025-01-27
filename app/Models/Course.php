2f3704450c116dbfe098f68ebee03121f11ae181
MAI_IT <mai.saleh.mu@gmail.com>
2025-01-27 11:02:30 +0300

    Add soft delete to course table

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Course extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     public function comments(){
         return $this->hasMany(Comment::class);
@@ -16,4 +17,12 @@ class Course extends Model
     public function registrations(){
         return $this->hasMany(Registration::class);
     }
+
+    protected $fillable = [ 
+        'title',
+        'price', 
+        'start_date',
+        'end_date',
+        'instructor_name' 
+    ]; 
 }
